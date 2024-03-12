4b13e41ee476c45638f12fd590cb2aa75fe2b229
Juan José González Abril <juanjo@gonzabril.es>
2024-03-12 08:51:15 +0100

    Implement solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,13 +1,26 @@
 package org.tfoc;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
+    private static final Map<Character, Character> pairs = Map.of('(', ')', '[', ']', '{', '}');
 
-    public boolean isValid(String s) {
-
-        return false;
+    public boolean isValid(String string) {
+        Deque<Character> open = new LinkedList<>();
+        return string.chars()
+            .mapToObj(i -> (char) i)
+            .allMatch(bracket -> {
+                if (pairs.containsKey(bracket)) {
+                    open.push(bracket);
+                    return true;
+                } else {
+                    return bracket.equals(pairs.get(open.pop()));
+                }
+            });
     }
-
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,33 @@
 package org.tfoc;
 
-import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-@Slf4j
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SolutionTest {
+    private Solution solution;
 
-    @Test
-    void testSolution() {
-
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
     }
 
+    @Test
+    void example1() {
+        var result = solution.isValid("()");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void example2() {
+        var result = solution.isValid("()[]{}");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void example3() {
+        var result = solution.isValid("(]");
+        assertThat(result).isFalse();
+    }
 }
